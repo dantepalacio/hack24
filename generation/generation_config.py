dbdb8760d551b5b7f8b98538d5067236d1df85e8
dantepalacio <anuar.beisenbaev2003@gmail.com>
2024-04-29 22:21:16 +0500

    added spam detection func

--- generation/generation_config.py
+++ generation/generation_config.py
@@ -5,7 +5,7 @@ import openai
 from dotenv import load_dotenv
 
 
-from generation.prompts.system_prompt import JSON_EXPLICIT_COMMENT_SYSTEM_PROMPT
+from prompts.system_prompt import JSON_EXPLICIT_COMMENT_SYSTEM_PROMPT, JSON_SPAM_COMMENT_SYSTEM_PROMPT
 
 
 load_dotenv()
@@ -13,7 +13,7 @@ load_dotenv()
 
 client = openai.OpenAI()
 
-def detect_explicit_comment(text:str) -> str:
+def detect_explicit_comment(text:str) -> json:
     '''Функция для определения запрещенных комментариев, принимает текст комментария
         text: Текст комментария
     '''
@@ -31,6 +31,24 @@ def detect_explicit_comment(text:str) -> str:
     return json.loads(answer)
 
 
+# экстрактно вытаскивать части коммента, которые являются спамом и передавать в json: status, reasons
+def detect_spam_comment(text: str) -> json:
+    '''Функция для определения спам сообщения или рекламы в сообщении'''
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+            {"role": "system", "content": JSON_SPAM_COMMENT_SYSTEM_PROMPT},
+            {"role": "user", "content": text}
+        ],
+        temperature=0.0,
+        max_tokens=2048,
+    )
+    
+    answer = response.choices[0].message.content
+    return answer
+
 if __name__ == '__main__':
-    text = 'голосуйте за путина'
-    print(detect_explicit_comment(text))
+    # text = 'голосуйте за путина'
+    # print(detect_explicit_comment(text))
+    text = ''''''
+    print(detect_spam_comment(text))
