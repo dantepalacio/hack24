d052275a5c1beb54429b07dc4a7564464cf65b46
dantepalacio <anuar.beisenbaev2003@gmail.com>
2024-04-30 23:20:55 +0500

    fixed utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,16 @@
 import sys
 import os
+import random
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-def explicit_content_check_from_text(result: str) -> bool:
+def generate_random_int_id(length=4):
+    min_value = 10**(length - 1)
+    max_value = (10**length) - 1
+    return random.randint(min_value, max_value)
+
+def explicit_content_check_from_text(result: dict) -> bool:
     explicit = 'publish'
     reasons = []
 
